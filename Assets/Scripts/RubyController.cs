0f32a8b99b5dc1941619e9ffd0f4446d9f62271f
luyumei <66385169+luyumei@users.noreply.github.com>
2020-07-08 12:20:11 +0800

    Initial commit

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,63 +1,101 @@
 ﻿// A Compiler is a tool that translates your code into a language that the computer can understand, because computers don’t read English!
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour // class是component
 // All GameObjects start with a Transform component that allows you to specify its position and rotation in the Scene
+// Because all GameObjects have a Transform component by default, Unity makes the transform variable available in all scripts
+// Transform is a variable too (created by Unity, not you), that contains the Transform component on the GameObject that the script is on
+// the Transform values in the Inspector that use x for the horizontal position, y for the vertical position and z for the depth
+// Think of the dot between transform and position as “contains”, so transform.position are retrieving the position that is “contained” inside the Transform
+
 {
+    public float speed = 3.0f; // “f”: This tells the computer that the number is a floating point number, so that it can store the number properly
+                               // Since all the Unity built-in scripts use floating points, you need to use floating point by adding an “f” to your decimal number
+    
+    public int maxHealth = 5;
+    
+    // if you make currentHealth public, you could change it to 10 in another script, even if the maxHealth is just 5.
+    // To avoid that, it is best to only allow changes through functions.
+    // This enables you to do checks — just like the ChangeHealth function,
+    // which uses a clamp to prevent health never going below 0 or above maxHealth.
+    // Properties are used like variables, but it only works like that when you READ it.
+    public int health { get { return currentHealth; }} // In the first block, you used the get keyword, to get whatever is in the second block.
+                                                       // only specified a “get” operation for health, so it’s read-only. 
+                                                       // The set keyword and works exactly like get, but makes the property writable.
+                                                       // The second block is just like a normal function, so you just return the currentHealth value.
+                                                       // The compiler handles this exactly like a function, so you can write anything you want in a function inside that get block
+                                                       // (such as declaring a variable, doing computations and calling other functions).
+    int currentHealth;
+    
+    private Rigidbody2D rigidbody2D; // store the Rigidbody
+    // a Rigidbody component has to be manually added to a GameObject,
+    // so Unity doesn’t have it as a built-in variable
+
+    private float horizontal;
+
+    private float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10; // This will make Unity render 10 frames per second
-                                            // cinema renders 24 frames per second
-                                            // The illusion of movement will be a bit broken, because you don’t have enough frames rendered(10/24)
+        rigidbody2D =
+            GetComponent<Rigidbody2D>(); // give you the Rigidbody2D that is attached to the same GameObject that your script is attached to, which is your character
+
+        currentHealth = maxHealth; // character starts with max Health
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10; // This will make Unity render 10 frames per second
+        // cinema renders 24 frames per second
+        // The illusion of movement will be a bit broken, because you don’t have enough frames rendered(10/24)
     }
 
-    // Update is called once per frame
+    // Update is called once per frame, i.e. every time the game computes a new image (an uncertain rate)
+    // It could be 20 images per second on a slow computer, or 3000 on a very fast one
     // To give the impression of movement, a game (just like a movie) is still images that are shown at high speed. Typically in games, 30 or 60 images show in one second. Each of those images is called a frame.
     // In this Update function, you will write anything you want to happen continuously in the game (for example, reading input from the player, moving GameObjects, or counting time passing).
     void Update()
     {
         // make the Ruby GameObject move
-        float
-            horizontal = Input.GetAxis("Horizontal"); // be ready to tie the character's movement to keyboard input
-                                                      // keyboard input through axes
-                                                      // It stores the result that Input.GetAxis ("Horizontal") provides
-                                                      // using the Unity Input System, which is composed of Input Settings and input code
-                                                      // which Unity gives you to query the value of an axis for that frame
+        horizontal = Input.GetAxis("Horizontal"); // be ready to tie the character's movement to keyboard input
+        // keyboard input through axes
+        // It stores the result that Input.GetAxis ("Horizontal") provides
+        // using the Unity Input System, which is composed of Input Settings and input code
+        // which Unity gives you to query the value of an axis for that frame
 
-        float
-            vertical = Input.GetAxis(
-                "Vertical"); // The default axis corresponding to the key up and down is called Vertical
+        vertical = Input.GetAxis(
+            "Vertical"); // The default axis corresponding to the key up and down is called Vertical
 
         //Debug.Log(horizontal); // Debug contains all functions that help debug your game
+    }
+
+    // For the physics computation to be stable, it needs to update at regular intervals (for example, every 16ms)
+    // shouldn’t read input in the Fixedupdate function
+    private void FixedUpdate()
+    {
         Vector2
-            position = transform
-                .position; // storing the current position contained in the Transform component inside a variable of type Vector2
+            position = rigidbody2D
+                .position; // use the Rigidbody itself instead of GameObject Transform (to avoid Ruby’s jittering)
+        // storing the current position contained in the Rigidbody component inside a variable of type Vector2
         // Think of Variables as boxes stored on a shelf with names written on the front, and inside are values
         // Types tell the computer what kind of data you want to store, so it can get the right amount of space in memory
         // Now there is a place in memory with the name “position” on it, which contains a copy of your GameObject’s current position
         // A Vector2 is a data type that stores two numbers, it “contains” x and y
-        
-        // Transform is a variable too (created by Unity, not you), that contains the Transform component on the GameObject that the script is on
-        // the Transform values in the Inspector that use x for the horizontal position, y for the vertical position and z for the depth
-        // Think of the dot between transform and position as “contains”, so here you are retrieving the position that is “contained” inside the Transform
+
         position.x =
             position.x +
-            3.0f * horizontal * Time.deltaTime; // “f”: This tells the computer that the number is a floating point number, so that it can store the number properly
-        // Since all the Unity built-in scripts use floating points, you need to use floating point by adding an “f” to your decimal number
-        
-        // it moves Ruby 0.1 units every time Unity calls the Update function, which means every FRAME
+            speed * horizontal *
+            Time.deltaTime;
+        // it moves Ruby %speed(=0.1) units every time Unity calls the Update function, which means every FRAME
         // If your game runs at 60 frames per second, Ruby will move at 0.1 * 60, so six units per second
         // But if the game runs at ten frames per second, Ruby only moves 0.1 * 10, so 1 unit per second!
         // If a player has a very old computer that can only run the game at 30 frames per second
         // and another player has a computer that can run it at 120 frames per second
         // those two players will have a main character that moves at radically different speeds
         // This will make the game either harder or easier to play, DEPENDING on the machine running the game
-        
+
         // kept the amount of movement to 0.1 * Time.deltaTime, so that means your character will move 0.1 units per SECOND.
         // It will take ten seconds to cover a single unit. That’s way too slow. A running character should cover three or four units per second
 
@@ -70,15 +108,29 @@ public class RubyController : MonoBehaviour // class是component
         // Your character now runs at the same speed on any machine, regardless of how many frames per second are used to render the game
         // It will just look choppier on slower machines. It’s now “frame INdependent”.
         // i.e. the character covers the same number of units per second as when the game ran faster
-        
+
 
         // it should only add it if the player presses the right arrow key on their keyboard
         // If you press left, the axis will be -1 (which makes the movement  position.x + -0.1f;). Ruby will move -0.1 to the left
         // If you press right, the axis will be 1. Ruby will move 0.1 to the right
         // If you press no key, the axis will be 0 and Ruby will not move (0.1 * 0 is 0)
         position.y =
-            position.y + 3.0f * vertical * Time.deltaTime; // The y value stored inside a Vector2 corresponds to the vertical position
-        transform.position =
-            position; // Before this line of code, you were only modifying a copy, like doing math on a side sheet of paper
+            position.y +
+            speed * vertical *
+            Time.deltaTime; // The y value stored inside a Vector2 corresponds to the vertical position
+        rigidbody2D.MovePosition(
+            position); // MovePosition (Vector2 newPosition)
+        // let the Physics System synchronize the GameObject position to the Rigidbody position
+        // Before this line of code, you were only modifying a copy, like doing math on a side sheet of paper
+    }
+
+    public void ChangeHealth(int amount) // function declaration (tells the compiler how to compile that function)    
+        // parameter is the amount of change to the Health
+    {
+        currentHealth =
+            Mathf.Clamp(currentHealth + amount, 0,
+                maxHealth); // Clamping ensures that the first parameter never goes lower than the second parameter
+        // and never goes above the third parameter
+        Debug.Log(currentHealth + "/" + maxHealth); // use + to merge strings together
     }
 }
